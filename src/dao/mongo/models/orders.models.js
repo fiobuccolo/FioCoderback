78c479de784ccfadb23008f0d1f73ea4397b9166
fiorella buccolo <fiobuccolo@gmail.com>
2023-08-29 21:12:08 +0200

    orders model

---
+++ src/dao/mongo/models/orders.models.js
@@ -0,0 +1,51 @@
+// Pendiente ver como autogenero el code-- era ++1
+//   
+//        
+//         # - purchase_Datetime: fecha y hora exacta en la que se formaliza la compra
+
+
+
+
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2"
+
+
+const ordersCollection = "orders";
+
+const orderSchema = new mongoose.Schema({
+    code: {
+        type:Number,
+        unique:true,
+    },
+    totalPrice: Number,
+    products: [{
+        _id:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref: 'Products'
+        },
+        quantity: Number,
+    }],
+    user:[{
+        _id:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref: 'Users'
+        },
+    }],
+}, {
+timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+}
+}
+);
+
+
+collectionSchema.pre("find", function () {
+    this.populate("products").populate("user");
+  });
+
+  orderSchema.plugin(mongoosePaginate)
+
+const OrderModel = mongoose.model(ordersCollection,orderSchema);
+
+export default OrderModel
--- src/router/carts.router.js
+++ src/router/carts.router.js
@@ -6,7 +6,7 @@ const cartsRouter = Router();
 cartsRouter.get("/", cartsController.getCarts)
 cartsRouter.get("/:id", cartsController.getCart)
 cartsRouter.post("/", cartsController.addCart); 
-//cartsRouter.patch('/:pid',cartsController.updateProduct)
+//cartsRouter.patch('/:pid',cartsController.updateProduct)  -- // solo el usuario puede agregar productos al carrito, agregar middleware --> passportCall("jwt"), authorization("user")
 //cartsRouter.delete('/:pid',cartsController.deleteProduct); 
 
 
